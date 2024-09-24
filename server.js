d0c6847b2bbca1ea38f05f8cd39ceb0d378f7114
Jmik Thang <jmikthang5@gmail.com>
2024-09-23 22:50:02 -0400

    deletion

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-const todos = [];
-const PORT = 3000;
+var todos = [];
+const PORT = 4000;
 
 app.use((req, rest, next) => {
   next();
@@ -27,10 +27,21 @@ app
     });
   })
   .post((req, res) => {
-    todos.push(req.body);
-    console.log(req.body);
+    todos.push(req.body); // body is object
+    console.log(req);
     res.redirect("/");
   });
+
+// Delete route
+
+app.route("/delete/:id").get((req, res) => {
+  const id = parseInt(req.params.id); // params is object (found in console when we log req)
+  console.log(req);
+  todos = todos.filter((todo, index) => {
+    return id !== index;
+  });
+  res.redirect("/");
+});
 
 // Server set up
 app.listen(PORT, () => {
